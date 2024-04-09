2ef6de1fede4b9c3cb3bcbf6ba5a6a1c47ab0f9b
Wajeeh007 <wajeeh.gillani335@gmail.com>
2024-04-09 11:08:35 +0500

    search timezone and bugs fix in add_timezone

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -7,6 +7,7 @@ const primaryGrey = Color(0xff4a4d55);
 const primaryDarkGrey = Color(0xff16151b);
 const primaryBlack = Color(0xff070609);
 const containerWhite = Color(0xfff6f8fa);
+const lightGrey = Color(0xff9e9e9e);
 
 class Constants {
 
